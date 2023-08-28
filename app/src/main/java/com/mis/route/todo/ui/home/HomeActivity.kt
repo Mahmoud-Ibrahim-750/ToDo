0b5d42caa6f9a21557ddfa9c46cdd94c8e51dfb2
Mahmoud-Ibrahim-750 <104442642+Mahmoud-Ibrahim-750@users.noreply.github.com>
2023-08-28 22:47:56 +0300

    * solved the tasks list issue regarding tasks that don't get displayed after addition. * solved the issue of unique id constrain and its crash when adding a new task.

--- app/src/main/java/com/mis/route/todo/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mis/route/todo/ui/home/HomeActivity.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import com.mis.route.todo.R
 import com.mis.route.todo.databinding.ActivityHomeBinding
 import com.mis.route.todo.ui.home.fragments.settings.SettingsFragment
+import com.mis.route.todo.ui.home.fragments.tasks.AddTaskBottomSheet
 import com.mis.route.todo.ui.home.fragments.tasks.TasksListFragment
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private var tasksListFragment = TasksListFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,23 +22,39 @@ class HomeActivity : AppCompatActivity() {
         showFragment(TasksListFragment())
 
         binding.bottomNavView.setOnItemSelectedListener {
-            showFragment(
                 when (it.itemId) {
-                R.id.tasks_nav -> TasksListFragment()
-                else -> SettingsFragment()
-            })
+                    R.id.tasks_nav -> {
+                        showFragment(tasksListFragment)
+                    }
+                    R.id.settings_nav -> showFragment(SettingsFragment())
+                }
             true
         }
 
+//        binding.bottomNavView.setOnItemSelectedListener {
+//            showFragment(
+//                when (it.itemId) {
+//                R.id.tasks_nav -> TasksListFragment()
+//                else -> SettingsFragment()
+//            })
+//            true
+//        }
+
         binding.addTaskFab.setOnClickListener {
-            AddTaskBottomSheet().show(supportFragmentManager, "AddTask")
+            val addTaskBottomSheet = AddTaskBottomSheet()
+            addTaskBottomSheet.onTaskAddedListener = AddTaskBottomSheet.OnTaskAddedListener {
+                val tasksListFragment = supportFragmentManager.findFragmentByTag("TasksListFragment") as TasksListFragment
+                tasksListFragment.loadTasks()
+                tasksListFragment.notifyTaskInsertionAtRear()
+            }
+            addTaskBottomSheet.show(supportFragmentManager, "AddTask")
         }
     }
 
     private fun showFragment(fragment: Fragment) {
         supportFragmentManager
             .beginTransaction()
-            .replace(binding.fragmentContainer.id, fragment)
+            .replace(binding.fragmentContainer.id, fragment, "TasksListFragment")
             .commit()
     }
 }
